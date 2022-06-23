becfd63e1cb59cfc0736b3434dff57686c919956
Nikita Kudenikov <KudenikovNS@yandex.ru>
2022-06-23 21:29:05 +0400

    Project_Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,7 +88,7 @@ module.exports = {
             query: `
               {
                 allMarkdownRemark(
-                  sort: { order: DESC },
+                  sort: { order: DESC, fields: [frontmatter___date] },
                 ) {
                   nodes {
                     excerpt
